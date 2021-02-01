3f9523907377e9d6f21b0e4e2439ab24b0d21363
Mateus Sponton <spntn.mateus@gmail.com>
2021-02-01 09:19:51 -0300

    test 3

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,63 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "test@imperioiluminacao.com.br"; // this is your Email address
-    $from = $_POST['test@imperioiluminacao.com.br']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $cont_email = $_POST['email'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $cont_email . " wrote the following:" . "\n\n" . $_POST['message'] . "Telefone :" . $phone;
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";a
+<?php
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name']; // Get Name value from HTML Form
+        $email_id = $_POST['email']; // Get Email Value
+        $mobile_no = $_POST['mobile']; // Get Mobile No
+        $msg = $_POST['message']; // Get Message Value
+         
+        $to = "test@imperioiluminacao.com.br"; // You can change here your Email
+        $subject = "'$name' has been sent a mail"; // This is your subject
+         
+        // HTML Message Starts here
+        $message ="
+        <html>
+            <body>
+                <table style='width:600px;'>
+                    <tbody>
+                        <tr>
+                            <td style='width:150px'><strong>Name: </strong></td>
+                            <td style='width:400px'>$name</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Email ID: </strong></td>
+                            <td style='width:400px'>$email_id</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Mobile No: </strong></td>
+                            <td style='width:400px'>$mobile_no</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Message: </strong></td>
+                            <td style='width:400px'>$msg</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        ";
+        // HTML Message Ends here
+         
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+ 
+        // More headers
+        $headers .= 'From: Admin <contato@imperioiluminacao.com.br>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
+        
+         
+        if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+            echo "<script>
+                    alert('Mail has been sent Successfully.');
+                </script>";
+        }
+ 
+        else{
+            // Message if mail has been not sent
+            echo "<script>
+                    alert('EMAIL FAILED');
+                </script>";
+        }
+    }
+?>
